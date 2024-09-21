2b8a91b9a7cc2985dd629752b28d92ccecb641f8
SillySkittle <108063779+SillySkittle@users.noreply.github.com>
2024-09-21 17:37:08 +0530

    changing the annotation drawing for player, referee and ball. changing the colour of annotation according to player jersey color. Team assignment  based on team color. interpolation of ball tracking.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from utils.video_utils import read_video, save_video
 from trackers import Tracker
+import cv2
+from team_assigner import TeamAssigner
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 def main():
     # Read Video
@@ -10,12 +14,44 @@ def main():
 
     tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
 
+    # # save cropped image of a player
+    # for track_id, player in tracks['players'][0].items():
+    #     print(track_id,player)
+    #     bbox = player['bbox']
+    #     print(bbox)
+    #     frame = video_frames[0]
+    #     # crop bbox from frame
+    #     cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+    #     #save the cropped image
+    #     cv2.imwrite(f'outpot_videos/cropped_img.jpg', cropped_image)
+    #     break
+
+    # Interpolate Ball Positions
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    
+
+
     # Draw output 
     ## Draw object Tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
      # Save video
-    save_video(output_video_frames, 'output_videos/output_video.avi')
+    save_video(output_video_frames, 'output_videos/output_video_interpolate_ball_positions.avi')
 
 if __name__ == '__main__':
+
     main()
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -7,11 +7,24 @@ sys.path.append('../')
 from utils.bbox_utils import get_center_of_bbox, get_bbox_width
 import cv2
 import numpy as np
+import pandas as pd
 
 class Tracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path) 
         self.tracker = sv.ByteTrack()
+
+    def interpolate_ball_positions(self,ball_positions):
+        ball_positions = [x.get(1,{}).get('bbox',[]) for x in ball_positions]
+        df_ball_positions = pd.DataFrame(ball_positions,columns=['x1','y1','x2','y2'])
+
+        # Interpolate missing values
+        df_ball_positions = df_ball_positions.interpolate()
+        df_ball_positions = df_ball_positions.bfill()
+
+        ball_positions = [{1: {"bbox":x}} for x in df_ball_positions.to_numpy().tolist()]
+
+        return ball_positions
 
     def detect_frames(self, frames):
         batch_size=20 
@@ -153,8 +166,8 @@ class Tracker:
 
             # Draw Players
             for track_id, player in player_dict.items():
-                #color = player.get("team_color",(0,0,255))
-                frame = self.draw_ellipse(frame, player["bbox"],(0,0,225), track_id)
+                color = player.get("team_color",(0,0,255))
+                frame = self.draw_ellipse(frame, player["bbox"],color, track_id)
 
             # Draw Referee
             for _, referee in referee_dict.items():
@@ -164,6 +177,6 @@ class Tracker:
             for track_id, ball in ball_dict.items():
                 frame = self.draw_traingle(frame, ball["bbox"],(0,255,0))
 
-                output_video_frames.append(frame)
+            output_video_frames.append(frame)
 
         return output_video_frames
